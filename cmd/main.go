6cf4bd73ac51a33cf65df7bbc34cdbf3dc8a6ddb
Ben Parees <bparees@redhat.com>
2018-09-13 15:47:00 -0400

    refactor cli invocation

---
+++ cmd/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"runtime"
+	"time"
+
+	"github.com/spf13/cobra"
+
+	"k8s.io/apiserver/pkg/util/logs"
+
+	"github.com/openshift/library-go/pkg/serviceability"
+
+	"github.com/openshift/builder/pkg/version"
+)
+
+func main() {
+	logs.InitLogs()
+	defer logs.FlushLogs()
+	defer serviceability.BehaviorOnPanic(os.Getenv("OPENSHIFT_ON_PANIC"), version.Get())()
+	defer serviceability.Profile(os.Getenv("OPENSHIFT_PROFILE")).Stop()
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	if len(os.Getenv("GOMAXPROCS")) == 0 {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+	}
+
+	basename := filepath.Base(os.Args[0])
+	command := CommandFor(basename)
+	if err := command.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+// CommandFor returns the appropriate command for this base name,
+// or the OpenShift CLI command.
+func CommandFor(basename string) *cobra.Command {
+	var cmd *cobra.Command
+
+	switch basename {
+	case "openshift-sti-build":
+		cmd = NewCommandS2IBuilder(basename)
+	case "openshift-docker-build":
+		cmd = NewCommandDockerBuilder(basename)
+	case "openshift-git-clone":
+		cmd = NewCommandGitClone(basename)
+	case "openshift-manage-dockerfile":
+		cmd = NewCommandManageDockerfile(basename)
+	case "openshift-extract-image-content":
+		cmd = NewCommandExtractImageContent(basename)
+	default:
+		fmt.Println("unknown command name: %s", basename)
+		os.Exit(1)
+	}
+
+	GLog(cmd.PersistentFlags())
+
+	return cmd
+}
