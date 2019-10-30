9869f7abd279161030dfe972ba2de7aa0f626163
Nalin Dahyabhai <nalin@redhat.com>
2019-10-30 10:56:33 -0400

    Proposed changes to update-deps.sh
    Use `go mod` to update our dependencies.  Currently expects whichever
    version of go is listed in go.mod to be the first one in $PATH.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -18,27 +18,7 @@ for pkg in "$GOPATH/src/k8s.io/kubernetes/staging/src/k8s.io/"*; do
   fi
 done
 
-# Some things we want in godeps aren't code dependencies, so ./...
-# won't pick them up.
-# TODO seems like this should be failing something somewhere
-#REQUIRED_BINS=(
-#  "github.com/elazarl/goproxy"
-#  "github.com/golang/mock/gomock"
-#  "github.com/containernetworking/cni/plugins/ipam/host-local"
-#  "github.com/containernetworking/cni/plugins/main/loopback"
-#  "k8s.io/code-generator/cmd/go-to-protobuf/protoc-gen-gogo"
-#  "k8s.io/code-generator/cmd/client-gen"
-#  "k8s.io/kubernetes/pkg/api/testing/compat"
-#  "k8s.io/kubernetes/test/e2e/generated"
-#  "github.com/onsi/ginkgo/ginkgo"
-#  "github.com/jteeuwen/go-bindata/go-bindata"
-#)
-
-glide update --strip-vendor
-
-# recreate symlinks after vendoring
-for pkg in vendor/k8s.io/kubernetes/staging/src/k8s.io/*; do
-  dir=$(basename $pkg)
-  rm -rf vendor/k8s.io/$dir
-  ln -s kubernetes/staging/src/k8s.io/$dir vendor/k8s.io/$dir
-done
+export GO111MODULE=on
+go mod tidy
+go mod vendor
+go mod verify
