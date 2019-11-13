4a57e85267fdf475e04cf155f70f95e8749d67ba
Nalin Dahyabhai <nalin@redhat.com>
2019-11-13 09:30:39 -0500

    hack/build.sh: use -mod=vendor
    Use -mod=vendor when building in hack/build.sh, so that it won't try to
    update dependencies, since that's hack/update-deps.sh's job.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- hack/lib/build/binaries.sh
+++ hack/lib/build/binaries.sh
@@ -8,6 +8,13 @@ function os::build::build_binary() {
   # Fetch the version.
   local version_ldflags
   version_ldflags=$(os::build::ldflags)
-  go build -ldflags "${version_ldflags}" -tags "${OS_GOFLAGS_TAGS-}" -o openshift-builder ./cmd
+  # test for go module support
+  local go_build
+  if go help mod >/dev/null 2>&1 ; then
+    go_build="env GO111MODULE=on go build -mod=vendor"
+  else
+    go_build="go build"
+  fi
+  $go_build -ldflags "${version_ldflags}" -tags "${OS_GOFLAGS_TAGS-}" -o openshift-builder ./cmd
 }
 readonly -f os::build::build_binary
