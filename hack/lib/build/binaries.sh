b32b13b9089ce1a1ef1dedfa23db5c60a0134d72
Nalin Dahyabhai <nalin@redhat.com>
2019-12-11 13:14:42 -0500

    bump dependencies
    Update to using the results of using go 1.13 for dependency management,
    and building using -mod=vendor.
    
    Update github.com/containers/storage to v1.15.3.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- hack/lib/build/binaries.sh
+++ hack/lib/build/binaries.sh
@@ -3,11 +3,25 @@
 # This library holds utility functions for building
 # and placing Golang binaries.
 
+# os::build::modvendorflag evaluates to either -mod=version or nothing
+function os::build::modvendorflag() {
+  tmpfile=${TMPDIR:-/tmp}/test${RANDOM}.go
+  trap 'rm -f "${tmpfile}"' EXIT
+  echo 'package main; func main() {}' > "${tmpfile}"
+  if go run -mod=vendor "${tmpfile}" ; then
+    echo "-mod=vendor"
+  fi
+}
+readonly -f os::build::modvendorflag
+
 # os::build::build_binary builds the openshift-builder binary
 function os::build::build_binary() {
   # Fetch the version.
   local version_ldflags
   version_ldflags=$(os::build::ldflags)
-  go build -ldflags "${version_ldflags}" -tags "${OS_GOFLAGS_TAGS-}" -o openshift-builder ./cmd
+  # Fetch additional build flags.
+  local mod_vendor_flag
+  mod_vendor_flag=$(os::build::modvendorflag)
+  go build ${mod_vendor_flag} -ldflags "${version_ldflags}" -tags "${OS_GOFLAGS_TAGS-}" -o openshift-builder ./cmd
 }
 readonly -f os::build::build_binary
