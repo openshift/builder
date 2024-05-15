85ff36193f0421391ebfad4d4b3986c0b839c31d
Adam Kaplan <adam.kaplan@redhat.com>
2024-05-15 14:55:15 -0400

    bump(openshift/source-to-image, containers/buildah):
    - Update s2i to v1.4.0
    - Update buildah to v1.35.4
    
    These dependency updates include a mitigation for
    CVE-2023-48795.
    See also https://issues.redhat.com/browse/OCPBUGS-25495

--- vendor/github.com/containers/common/pkg/apparmor/apparmor_linux.go
+++ vendor/github.com/containers/common/pkg/apparmor/apparmor_linux.go
@@ -1,5 +1,4 @@
 //go:build linux && apparmor
-// +build linux,apparmor
 
 package apparmor
 
@@ -211,6 +210,11 @@ func parseAAParserVersion(output string) (int, error) {
 	lines := strings.SplitN(output, "\n", 2)
 	words := strings.Split(lines[0], " ")
 	version := words[len(words)-1]
+
+	// trim "-beta1" suffix from version="3.0.0-beta1" if exists
+	version = strings.SplitN(version, "-", 2)[0]
+	// also trim "~..." suffix used historically (https://gitlab.com/apparmor/apparmor/-/commit/bca67d3d27d219d11ce8c9cc70612bd637f88c10)
+	version = strings.SplitN(version, "~", 2)[0]
 
 	// split by major minor version
 	v := strings.Split(version, ".")
