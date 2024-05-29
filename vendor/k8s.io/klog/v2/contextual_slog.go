995d4fc941e0f5a7b6c4d3e6249d45e0589b5028
apoorvajagtap <apoorvajagtap4@gmail.com>
2024-05-29 17:39:49 +0530

    bumps openshift/api: OCPBUGS-33215

--- vendor/k8s.io/klog/v2/contextual_slog.go
+++ vendor/k8s.io/klog/v2/contextual_slog.go
@@ -1,5 +1,8 @@
+//go:build go1.21
+// +build go1.21
+
 /*
-Copyright 2022 The Kubernetes Authors.
+Copyright 2021 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,12 +17,15 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package internal
+package klog
 
-// Used by tests to selectively disable experimental JSON unmarshaler
-var UseOptimizedJSONUnmarshaling bool = true
-var UseOptimizedJSONUnmarshalingV3 bool = true
+import (
+	"log/slog"
 
-// Used by tests to selectively disable experimental JSON marshaler
-var UseOptimizedJSONMarshaling bool = true
-var UseOptimizedJSONMarshalingV3 bool = true
+	"github.com/go-logr/logr"
+)
+
+// SetSlogLogger reconfigures klog to log through the slog logger. The logger must not be nil.
+func SetSlogLogger(logger *slog.Logger) {
+	SetLoggerWithOptions(logr.FromSlogHandler(logger.Handler()), ContextualLogger(true))
+}
